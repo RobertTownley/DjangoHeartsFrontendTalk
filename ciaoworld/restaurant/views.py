5ce94460299a38880b54f583e77da0a4708a36eb
Robert Townley <Me@RobertTownley.com>
2018-07-29 13:21:44 -0400

    Template work

--- ciaoworld/restaurant/views.py
+++ ciaoworld/restaurant/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class HomeView(TemplateView):
+    template_name = 'restaurant/home.html'
+
+
+class AboutView(TemplateView):
+    template_name = 'restaurant/about.html'
+
+
+class ContactView(TemplateView):
+    template_name = 'restaurant/contact.html'
+
+
+class MenuView(TemplateView):
+    template_name = 'restaurant/menu.html'
