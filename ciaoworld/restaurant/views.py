09ab90a6070e105275d98aa4508fe1cf2b1b14c4
Robert Townley <Me@RobertTownley.com>
2018-07-29 17:58:53 -0400

    Scss styles for homepage and namespacing views

--- ciaoworld/restaurant/views.py
+++ ciaoworld/restaurant/views.py
@@ -7,13 +7,28 @@ from restaurant.models import Dish
 class HomeView(TemplateView):
     template_name = 'restaurant/home.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['page'] = 'home'
+        return context
+
 
 class AboutView(TemplateView):
     template_name = 'restaurant/about.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(AboutView, self).get_context_data(**kwargs)
+        context['page'] = 'about'
+        return context
+
 
 class ContactView(TemplateView):
     template_name = 'restaurant/contact.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ContactView, self).get_context_data(**kwargs)
+        context['page'] = 'contact'
+        return context
 
 
 class MenuView(TemplateView):
@@ -21,6 +36,7 @@ class MenuView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(MenuView, self).get_context_data(**kwargs)
+        context['page'] = 'menu'
         for category in DISH_CATEGORIES:
             database_category = category[0]
             context[database_category] = Dish.objects.filter(category=database_category)
